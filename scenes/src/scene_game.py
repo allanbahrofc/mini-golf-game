77d63b993a820f62944d853e0a1b62b90cd7a3a7
Allan Bruno de Souza Bahr <allanbrunodesouzabahr@gmail.com>
2023-06-19 18:15:47 -0300

    Added keyboard keys

--- scenes/src/scene_game.py
+++ scenes/src/scene_game.py
@@ -1,5 +1,6 @@
-from scene_configuration import config, colors, sprites, walls
+from scene_configuration import config, colors, sprites, walls,keys
 import pyglet
+
 
 # Display
 displayWidth = config['width']
@@ -9,14 +10,27 @@ display.set_caption('Mini Golf')
 
 # Sprites
 ballSprite = pyglet.image.load(sprites['ballOneSprite'])
-ball = pyglet.sprite.Sprite(ballSprite, 0, 0)
 background = pyglet.shapes.Rectangle(0,0,displayWidth,displayHeight,colors['lightGreen'])
+
+# Instancies
+ball = pyglet.sprite.Sprite(ballSprite, 0, 0)
 
 @display.event
 def on_draw():
     display.clear()
     background.draw()
     ball.draw()
+
+@display.event
+def on_key_press(symbol, modifiers):
+    if(symbol == keys['upArrow']):
+        print('going up to the sky')
+    elif(symbol == keys['downArrow']):
+        print('going down to the ground')
+    elif(symbol == keys['leftArrow']):
+        print('going to the left')
+    elif(symbol == keys['rightArrow']):
+        print('going to the right')
 pyglet.app.run()
 
 #TODO
