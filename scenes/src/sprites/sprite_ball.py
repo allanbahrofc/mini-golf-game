75dcdd97e4f9a48c9097b58788b14424f26650d0
Allan Bruno de Souza Bahr <allanbrunodesouzabahr@gmail.com>
2023-06-19 17:59:41 -0300

    v1.0.1

--- scenes/src/sprites/sprite_ball.py
+++ scenes/src/sprites/sprite_ball.py
@@ -1,7 +1,3 @@
-from scene_configuration import sprites, config, colors
-import pyglet
-class Ball(pyglet.sprite.Sprite):
-    def __init__(self, img, x=0, y=0):
-        self.image = img
-        self.x = x
-        self.y = y
+"""
+Futures updates to implement a sprites libs
+"""
