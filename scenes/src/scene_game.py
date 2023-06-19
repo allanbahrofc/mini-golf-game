0ae38756734c4cacf45947124c7b6e4bf1c5d604
Allan Bruno de Souza Bahr <allanbrunodesouzabahr@gmail.com>
2023-06-19 17:56:54 -0300

    version 1.0.1

--- scenes/src/scene_game.py
+++ scenes/src/scene_game.py
@@ -1,4 +1,5 @@
 from scene_configuration import config, colors, sprites, walls
+from sprites.sprite_ball import Ball
 import pyglet
 
 # Display
@@ -8,12 +9,14 @@ display = pyglet.window.Window(displayWidth, displayHeight)
 display.set_caption('Mini Golf')
 
 # Sprites
+Ball(pyglet.image.load(sprites['ballOneSprite']),0, 0)
 background = pyglet.shapes.Rectangle(0,0,displayWidth,displayHeight,colors['lightGreen'])
 
 @display.event
 def on_draw():
     display.clear()
     background.draw()
+    Ball.draw()
 pyglet.app.run()
 
 #TODO
--- scenes/src/sprites/sprite_ball.py
+++ scenes/src/sprites/sprite_ball.py
@@ -1,6 +1,7 @@
+from scene_configuration import sprites, config, colors
 import pyglet
-
 class Ball(pyglet.sprite.Sprite):
     def __init__(self, img, x=0, y=0):
-        super().__init__(img, x, y)
-
+        self.image = img
+        self.x = x
+        self.y = y
