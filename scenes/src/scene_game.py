ea3239d7935fc40ea48b220ea6ede63f79bbcc61
Allan Bruno de Souza Bahr <allanbrunodesouzabahr@gmail.com>
2023-08-11 16:35:33 -0300

    movement working and collision too

--- scenes/src/scene_game.py
+++ scenes/src/scene_game.py
@@ -1,15 +1,7 @@
-from scene_configuration import config, colors, sprites, walls
 import pygame
 import random
-from pygame.locals import (
-    K_UP,
-    K_DOWN,
-    K_LEFT,
-    K_RIGHT,
-    K_ESCAPE,
-    KEYDOWN,
-    QUIT,
-)
+from scene_configuration import config, colors, sprites, walls
+from pygame.locals import *
 
 pygame.init()
 title = pygame.display.set_caption('Mini Golf')
@@ -18,41 +10,56 @@ screen = pygame.display.set_mode([config['width'], config['height']])
 # Instance (player)
 player_sprite = pygame.image.load(sprites['ballOneSprite'])
 player_pos = pygame.Vector2(config['windowMain'])
+player_dir = None
+player_velocity = 0.4
 
 # Instance (holes)
 holes_sprite = pygame.image.load(sprites['holeOneSprite'])
 holes_pos = pygame.Vector2(random.randint(
-    0, config['width']-40), random.randint(0, config['height'])-20)
+    0, config['width']-140), random.randint(0, config['height'])-120)
 
-# Instance (walls) TODO
+# Instance (walls)
 wall_background_sprite = pygame.image.load(walls['background'])
-wall_background_pos = pygame.Vector2(100,100)
+wall_background_pos = pygame.Vector2(100, 100)
 
 clock = pygame.time.Clock()
 isRunning = True
 while isRunning:
     for event in pygame.event.get():
-        if event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
-                isRunning = False
-            elif event.key == K_UP:
-                player_pos.y -= 5*1.5
-            elif event.key == K_DOWN:
-                player_pos.y += 5*1.5
-            elif event.key == K_LEFT:
-                player_pos.x -= 5*1.5
-            elif event.key == K_RIGHT:
-                player_pos.x += 5*1.5
-
-        elif event.type == QUIT:
+        if event.type == QUIT:
             isRunning = False
-        screen.fill(colors['darkGreen'])
-        wallBehavior = screen.blit(wall_background_sprite, wall_background_pos)
-        holeBehavior = screen.blit(holes_sprite, holes_pos)
-        playerBehavior = screen.blit(player_sprite, player_pos)
         
-        # Introduce [Collision] TODO
-        if player.collidepoint(hole.centerx, hole.centery):
-            print('Collision')
+    keys = pygame.key.get_pressed()
+
+    # Keys
+    if keys[K_UP] or keys[K_w]:
+        player_dir = 'UP'
+
+    elif keys[K_LEFT] or keys[K_a]:
+        player_dir = 'LEFT'
+
+    elif keys[K_RIGHT] or keys[K_d]:
+        player_dir = 'RIGHT'
+
+    elif keys[K_DOWN] or keys[K_s]:
+        player_dir = 'DOWN'
+
+    # Directions
+    if player_dir == 'UP':
+        player_pos.y -= 1*player_velocity
+    elif player_dir == 'DOWN':
+        player_pos.y += 1*player_velocity
+    elif player_dir == 'LEFT':
+        player_pos.x -= 1*player_velocity
+    elif player_dir == 'RIGHT':
+        player_pos.x += 1*player_velocity
+    
+    screen.fill(colors['darkGreen'])
+    # wallBehavior = screen.blit(wall_background_sprite, wall_background_pos)
+    holeBehavior = screen.blit(holes_sprite, holes_pos)
+    playerBehavior = screen.blit(player_sprite, player_pos)
+    # Introduce [Collision]
+    if playerBehavior.collidepoint(holeBehavior.centerx, holeBehavior.centery):
+        print('Collision')
     pygame.display.update()
     clock.tick(60)
