ff0b856a0db6a73000eaba5c5061f6f78073b737
Allan Bruno de Souza Bahr <allanbrunodesouzabahr@gmail.com>
2023-08-11 17:24:42 -0300

    movement working and collision too

--- scenes/src/entity/entityPlayer.py
+++ scenes/src/entity/entityPlayer.py
@@ -1,6 +1,6 @@
-import pygame
 from pygame.locals import *
-from ..scene_configuration import sprites, config
+from scene_configuration import *
+import pygame
 
 class entityPlayer(pygame.sprite.Sprite):
 
@@ -8,30 +8,35 @@ class entityPlayer(pygame.sprite.Sprite):
         self.x = x
         self.y = y
         self.pos = pygame.Vector2(self.x, self.y)
-        self.image = pygame.image.load(sprites['ballOneSprite'])        
-        self.rect = pygame.Surface.get_rect(self.image)
+        self.sprite = pygame.image.load(sprites['ballOneSprite'])
+        self.rect = pygame.Surface.get_rect(self.sprite)
+        self.velocity = 1.2
         self.direction = None
 
     def move(self):
         keys = pygame.key.get_pressed()
-    
+
         if keys[K_UP]:
             self.direction = 'UP'
         elif keys[K_DOWN]:
             self.direction = 'DOWN'
         elif keys[K_LEFT]:
-            self.direction = 'LEFT'        
+            self.direction = 'LEFT'
         elif keys[K_RIGHT]:
             self.direction = 'RIGHT'
 
         if self.direction == 'UP':
-            self.pos.y -= 1
+            self.pos.y -= 1*self.velocity
         elif self.direction == 'DOWN':
-            self.pos.y += 1
+            self.pos.y += 1*self.velocity
         elif self.direction == 'LEFT':
-            self.pos.x -= 1
+            self.pos.x -= 1*self.velocity
         elif self.direction == 'RIGHT':
-            self.pos.x += 1
+            self.pos.x += 1*self.velocity
+
+    def checkCollision(self, collider):
+        if self.rect.colliderect(collider):
+            print('collider')
 
     def update(self):
         self.move()
--- scenes/src/scene_game.py
+++ scenes/src/scene_game.py
@@ -1,26 +1,22 @@
-import pygame
-import random
-from scene_configuration import config, colors, sprites, walls
+from entity.entityPlayer import entityPlayer
 from pygame.locals import *
-
+from scene_configuration import config, colors, sprites, walls
+import random
+import pygame
 pygame.init()
 title = pygame.display.set_caption('Mini Golf')
 screen = pygame.display.set_mode([config['width'], config['height']])
 
-# Instance (player)
-player_sprite = pygame.image.load(sprites['ballOneSprite'])
-player_pos = pygame.Vector2(config['windowMain'])
-player_dir = None
-player_velocity = 0.4
+playerBehavior = entityPlayer(0, 0)
 
 # Instance (holes)
 holes_sprite = pygame.image.load(sprites['holeOneSprite'])
 holes_pos = pygame.Vector2(random.randint(
-    0, config['width']-140), random.randint(0, config['height'])-120)
+    0, config['width']), random.randint(0, config['height']))
 
 # Instance (walls)
-wall_background_sprite = pygame.image.load(walls['background'])
-wall_background_pos = pygame.Vector2(100, 100)
+# wall_background_sprite = pygame.image.load(walls['background'])
+# wall_background_pos = pygame.Vector2(100, 100)
 
 clock = pygame.time.Clock()
 isRunning = True
@@ -29,37 +25,14 @@ while isRunning:
         if event.type == QUIT:
             isRunning = False
 
-    keys = pygame.key.get_pressed()
-
-    # Keys
-    if keys[K_UP] or keys[K_w]:
-        player_dir = 'UP'
-
-    elif keys[K_LEFT] or keys[K_a]:
-        player_dir = 'LEFT'
-
-    elif keys[K_RIGHT] or keys[K_d]:
-        player_dir = 'RIGHT'
-
-    elif keys[K_DOWN] or keys[K_s]:
-        player_dir = 'DOWN'
-
-    # Directions
-    if player_dir == 'UP':
-        player_pos.y -= 1*player_velocity
-    elif player_dir == 'DOWN':
-        player_pos.y += 1*player_velocity
-    elif player_dir == 'LEFT':
-        player_pos.x -= 1*player_velocity
-    elif player_dir == 'RIGHT':
-        player_pos.x += 1*player_velocity
-
     screen.fill(colors['darkGreen'])
-    # wallBehavior = screen.blit(wall_background_sprite, wall_background_pos)
     holeBehavior = screen.blit(holes_sprite, holes_pos)
-    playerBehavior = screen.blit(player_sprite, player_pos)
+    screen.blit(playerBehavior.sprite, playerBehavior.pos)
+    # wallBehavior = screen.blit(wall_background_sprite, wall_background_pos)
+    # playerBehavior = screen.blit(player_sprite, player_pos)
+
     # Introduce [Collision]
-    if playerBehavior.collidepoint(holeBehavior.centerx, holeBehavior.centery):
-        print('Collision')
+    playerBehavior.checkCollision(holeBehavior)
+    playerBehavior.move()
     pygame.display.update()
     clock.tick(60)
